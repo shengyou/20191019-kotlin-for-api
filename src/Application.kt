bb48396bc169ed15e3fb5861f71a940fbf12b20d
Shengyou Fan <shengyoufan@gmail.com>
2019-10-18 16:39:19 +0800

    respond JSON

--- src/Application.kt
+++ src/Application.kt
@@ -17,7 +17,8 @@ fun Application.module(testing: Boolean = false) {
     routing {
 
         get("/") {
-            call.respondText("Hello, world", ContentType.Text.Plain, HttpStatusCode.OK)
+            //language=JSON
+            call.respondText("{\n  \"result\": true,\n  \"data\": [\n    {\n      \"id\": 1,\n      \"title\": \"Item 1\"\n    },\n    {\n      \"id\": 2,\n      \"title\": \"Item 2\"\n    }\n  ]\n  \n}", ContentType.Application.Json, HttpStatusCode.OK)
         }
 
     }
